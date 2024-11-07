7b2b7524c203b4e146feb4ac298c573a1a1fb678
IforCU <tmdgus0844@gmail.com>
2024-11-08 00:37:54 +0900

    [FIX] - add annotation, and fixed bug

--- src/main/java/com/Stomp/WebSocket/Controller/WebSocketController.java
+++ src/main/java/com/Stomp/WebSocket/Controller/WebSocketController.java
@@ -14,6 +14,6 @@ public class WebSocketController {
     @SendTo("/topic/greetings")
     public Greeting greeting(Message message) throws Exception {
         Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        return new Greeting(HtmlUtils.htmlEscape(message.getName()));
     }
 }
